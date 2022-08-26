380f94779fa7d466f1fe8c6cc195ffe91c95b992
TheWisePigeon <zozozozeph@gmail.com>
2022-08-26 21:49:38 +0000

    added skills section2

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,6 +1,10 @@
 <div class=" flex flex-col justify-start  h-screen m-auto w-[900px] text-lg">
-    <div class=" text-left text-4xl font-bold py-4">
-        Joseph DOGBEVI
+    <div class=" text-left flex justify-between items-baseline text-4xl font-bold py-4">
+        <p>Joseph DOGBEVI</p>
+        <div class=" flex justify-between w-52 text-lg">
+            <button class=" border-2 border-white hover:bg-white hover:text-slate-600 p-1"><a href="/projects">PROJECTS</a></button>
+            <button class=" border-2 border-white hover:bg-white hover:text-slate-600 p-1"><a href="/blog">BLOG</a></button>
+        </div>
     </div>
     <hr >
     <div class=" mb-14">
