819eb023f82b4722a099be8b311dd6b090983bea
TheWisePigeon <zozozozeph@gmail.com>
2022-08-26 21:53:27 +0000

    ready for projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,3 +1,18 @@
-<div class=" bg-slate-600 flex flex-col justify-start items-center h-screen">
-    aight
+<div class=" flex flex-col justify-start  h-screen m-auto w-[900px] text-lg">
+  <div
+    class=" text-left flex justify-between items-baseline text-4xl font-bold py-4"
+  >
+    <p>Joseph DOGBEVI</p>
+    <div class=" flex justify-between w-52 text-lg">
+      <button
+        class=" border-2 border-white hover:bg-white hover:text-slate-600 p-1"
+        ><a href="/blog">BLOG</a></button
+      >
+      <button
+        class=" border-2 border-white hover:bg-white hover:text-slate-600 p-1"
+        ><a href="/resume">RESUME</a></button
+      >
+    </div>
+  </div>
+  <hr />
 </div>
