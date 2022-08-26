5a25425c01067f3f408987c7fe52b32d83f577f4
TheWisePigeon <zozozozeph@gmail.com>
2022-08-26 08:12:00 +0000

    added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<div class=" bg-red-500">
-    okokok
+<div class=" bg-red-500 h-screen w-full">
+    <div class=" flex  justify-center w-3/12">
+        <div>Resume</div>
+        <div>Projects</div>
+        <div>Blog</div>
+    </div>
 </div>
