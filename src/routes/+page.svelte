ddf1ab31ed6ce20be5f4818b05ac96d3023fa873
TheWisePigeon <zozozozeph@gmail.com>
2022-08-26 09:47:18 +0000

    added landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,34 @@
-<div class=" bg-red-500 h-screen w-full">
-    <div class=" flex  justify-center w-3/12">
-        <div>Resume</div>
-        <div>Projects</div>
-        <div>Blog</div>
+<div
+  class=" bg-slate-600 h-screen w-full absolute flex items-center justify-center"
+>
+  <div class=" flex flex-col m-auto justify-between w-full p-3 space-y-10">
+    <div class=" m-auto font-bold text-8xl ">Joseph DOGBEVI</div>
+    <hr class=" m-auto w-40" />
+    <div class=" text-center">FullStack Web Developer</div>
+    <div class=" flex w-96 m-auto justify-between">
+      <div>
+        <button
+          class=" border-white border-2 p-3 hover:bg-white hover:text-slate-600"
+        >
+          <a href="/resume">RESUME</a>
+        </button>
+      </div>
+      <div>
+        <button
+          class=" border-white border-2 p-3 hover:bg-white hover:text-slate-600"
+        >
+          <a href="/projects">PROJECTS</a>
+        </button>
+      </div>
+      <div>
+        <button
+          class=" border-white border-2 p-3 hover:bg-white hover:text-slate-600"
+        >
+          <a href="/blog">BLOG</a>
+        </button>
+      </div>
     </div>
+
+    <div />
+  </div>
 </div>
