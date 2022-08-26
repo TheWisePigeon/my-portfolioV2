43c1cceb81e7cbd6c6f335c8ec1932d2a723d798
TheWisePigeon <zozozozeph@gmail.com>
2022-08-26 21:28:54 +0000

    added skills section

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -2,19 +2,41 @@
     <div class=" text-left text-4xl font-bold py-4">
         Joseph DOGBEVI
     </div>
-    <hr>
-    <div class=" mb-5">
+    <hr >
+    <div class=" mb-14">
         I am Joseph, a 20 years old FullStack Web Developer. <br>
         I have a passion for technology and software development/architecture. <br>
         I am always learning new things and improving my skills to become a better developer. <br>
         In my free time, I enjoy building open source projects, write, read or play video games :) <br>
     </div>
     <hr>
-    <div>
+    <div class=" mb-14">
         Connect with me: <br>
         <p  >Email: <a class="text-green-500 underline" href="mailto:josephdogbevi2002@gmail.com" >josephdogbevi2002@gmail.com</a></p>
         <p  >Github: <a class="text-green-500 underline" href="github.com/TheWisePigeon">@TheWisePigeon</a></p>
         <p  >Twitter: <a class="text-green-500 underline" href="https://twitter.com/pigeondev0_0">@pigeondev0_0</a></p>
         <p  >Blogs: <a class="text-green-500 underline" href="https://pigeonscript.co">PigeonScript</a>  <a class="text-green-500 underline" href="https://pigeondev-blog.vercel.app">PigeonDev</a> </p>
+    </div>
+    <hr>
+    <div class=" mb-14">
+        <p>My Skills</p>
+        <div>
+            <p class=" font-bold inline">Languages:</p> JavaScript TypeScript Python
+        </div>
+        <div>
+            <p class=" font-bold inline">Frontend Development:</p> React, Svelte, Vue
+        </div>
+        <div>
+            <p class=" font-bold inline">Backend Development:</p> ExpressJS, tRPC, Flask
+        </div>
+        <div>
+            <p class="font-bold inline">Databases:</p> MongoDB, PostgreSQL, Firebase
+        </div>
+        <div>
+            <p class=" font-bold inline">Hosting:</p> Firebase, Vercel, Netlify, Linode
+        </div>
+        <div>
+            <p class=" font-bold inline">More:</p> Flutter, Tauri, Machine Learning
+        </div>
     </div>
 </div>
