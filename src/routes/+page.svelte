e98fa53a4dc791580ec60495a2c37981cf4b7f8b
TheWisePigeon <zozozozeph@gmail.com>
2022-09-03 10:32:39 +0000

    style: rebuilding for responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,3 @@
-<div
-  class=" bg-slate-600 h-screen w-full absolute flex items-center justify-center bg-no-repeat bg-cover" style="background-image: url('bg2.gif');"
->
-  <div class=" flex flex-col m-auto justify-between w-full p-3 space-y-10">
-    <div class=" m-auto font-bold text-8xl ">Joseph DOGBEVI</div>
-    <hr class=" m-auto w-40" />
-    <div class=" text-center text-lg">FullStack Web Developer</div>
-    <div class=" flex w-96 m-auto justify-between">
-      <div>
-        <button
-          class=" border-white border-2 p-3 w-24 hover:bg-white hover:text-slate-600"
-        >
-          <a href="/resume">RESUME</a>
-        </button>
-      </div>
-      <div>
-        <button
-          class=" border-white border-2 p-3 w-24 hover:bg-white hover:text-slate-600"
-        >
-          <a href="/projects">PROJECTS</a>
-        </button>
-      </div>
-      <div>
-        <button
-          class=" border-white border-2 p-3 w-24 hover:bg-white hover:text-slate-600"
-        >
-          <a href="/blog">BLOG</a>
-        </button>
-      </div>
-    </div>
-
-    <div />
-  </div>
+<div>
+  Bruh
 </div>
