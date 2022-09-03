ec77d0a3ac1d08ffe6e1b290790e715f0fb917f5
TheWisePigeon <zozozozeph@gmail.com>
2022-09-03 23:29:27 +0000

    added responsive navbar

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,3 +1,14 @@
 <div>
-    bruh
+    <div class=" flex justify-between p-2 items-baseline">
+        <a href="/" class=" text-2xl">Joseph DOGBEVI</a>
+        <div class=" flex justify-between space-x-2 ">
+            <a href="/projects">
+                <button class=" border-2 border-white hover:bg-white hover:text-slate-600 p-2 ">Projects</button>
+            </a>
+            <a href="/blog">
+                <button class=" border-2 border-white hover:bg-white hover:text-slate-600 p-2 ">Blog</button>
+            </a>
+        </div>
+    </div>
+    <hr>
 </div>
