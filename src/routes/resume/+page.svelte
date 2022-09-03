eee8e4e16d62237aa22c4071e2ee9f5433a721c6
TheWisePigeon <zozozozeph@gmail.com>
2022-09-03 23:49:27 +0000

    added contacts section

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,4 +1,4 @@
-<div>
+<div class=" p-4">
     <div class=" flex justify-between p-2 items-baseline">
         <a href="/" class=" text-2xl">Joseph DOGBEVI</a>
         <div class=" flex justify-between space-x-2 ">
@@ -12,9 +12,10 @@
     </div>
     <hr>
     <div class=" p-2 text-justify">
-        I am Joseph. A 20 years old FullStack Web Developer, Machine learning enthusiast and Technical writer
+        I am Joseph. A 20 years old FullStack Web Developer, Machine learning enthusiast and Technical writer.
     </div>
-    <div>
-        
-    </div>
+    <p class=" block">Email: <a class=" text-red-500" href="mailto:josephdogbevi2002@gmail.com">josephdogbevi2002@gmail.com</a> </p>
+    <p class=" block">Github: <a class=" text-red-500" href="https://github.com/TheWisePigeon">@TheWisePigeon</a> </p>
+    <p class=" block">Twitter: <a class=" text-red-500" href="https://twitter.com/pigeondev0_0">@pigeondev0_0</a> </p>
+    <p class=" block">NPM: <a class=" text-red-500" href="https://npmjs.com/the_wise_pigeon">@the_wise_pigeon</a> </p>
 </div>
