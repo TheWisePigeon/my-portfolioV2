42b5c6bf3a2e550d03f543dd21169841c1990418
TheWisePigeon <zozozozeph@gmail.com>
2022-09-04 00:20:51 +0000

    added experience

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -14,10 +14,10 @@
     <div class="text-justify mb-3">
         I am Joseph. A 20 years old FullStack Web Developer, Machine learning enthusiast and Technical writer.
     </div>
-    <p class=" block">Email: <a class=" text-red-500" href="mailto:josephdogbevi2002@gmail.com">josephdogbevi2002@gmail.com</a> </p>
-    <p class=" block">Github: <a class=" text-red-500" href="https://github.com/TheWisePigeon">@TheWisePigeon</a> </p>
-    <p class=" block">Twitter: <a class=" text-red-500" href="https://twitter.com/pigeondev0_0">@pigeondev0_0</a> </p>
-    <p class=" block">NPM: <a class=" text-red-500" href="https://npmjs.com/the_wise_pigeon">@the_wise_pigeon</a> </p>
+    <p class=" block">Email: <a class=" underline text-red-500" href="mailto:josephdogbevi2002@gmail.com">josephdogbevi2002@gmail.com</a> </p>
+    <p class=" block">Github: <a class=" underline text-red-500" href="https://github.com/TheWisePigeon">TheWisePigeon</a> </p>
+    <p class=" block">Twitter: <a class=" underline text-red-500" href="https://twitter.com/pigeondev0_0">pigeondev0_0</a> </p>
+    <p class=" block">NPM: <a class=" underline text-red-500" href="https://npmjs.com/the_wise_pigeon">the_wise_pigeon</a> </p>
 
     <hr class=" my-4">
     <div class=" space-y-3">
@@ -29,7 +29,7 @@
             <p class="font-bold inline">Frontend:</p>React, NextJS, Svelte 
         </div>
         <div>
-            <p class="font-bold inline">Backend:</p>ExpressJS, Flask
+            <p class="font-bold inline">Backend:</p>NodeJS, Express, Flask
         </div>
         <div>
             <p class="font-bold inline">Databases:</p>Firebase, PostgreSQL, NoSQL 
@@ -39,6 +39,24 @@
         </div>        
         <div>
             <p class="font-bold inline">More:</p>Tauri | Electron, Machine Learning 
+        </div>
+    </div>
+    <hr class=" my-4">
+    <div>
+        <p class=" font-bold">Projects</p>
+        Check my projects <a class=" text-red-500" href="/projects">Here</a>
+    </div>
+    <hr class=" my-4">
+    <div>
+        <p class=" font-bold">Experience</p>
+        <div class=" my-2">
+            <p class=" font-bold">A2B Tech</p>
+            <p class=" inline underline">Web Development Internship</p>(June 2022-August 2022): Web Development consulting. Maintained and created services using TypeScript, Express and React <br>
+            <p class=" inline underline">Web Development</p>(September 2022-Now): Web Development consulting. 
+        </div>
+        <div class=" my-2">
+            <p class=" font-bold">AXETAG SAS</p>
+            <p class=" inline underline">Backend Development</p>(May 2022- Now): Backend Development with Flask and Machine Learning.
         </div>
     </div>
 </div>
